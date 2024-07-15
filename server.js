9094136d0cc0239094eac3823fb8707688a7c776
Kevin Dela Paz <kevindelapaz21@gmail.com>
2024-07-15 11:26:29 -0500

    added database

--- server.js
+++ server.js
@@ -1,49 +1,27 @@
 import express from "express"
 const app = express()
+import morgan from "morgan"
+import mongoose from "mongoose"
+
+// Route files
 import bootcampRouter from "./routes/bootcampRouter.js"
 
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan('dev'))
+}
+
+// Mount routers 
 app.use("/api/v1/bootcamps", bootcampRouter)
 
-app.get("/api/v1/courses", (req, res) => {
-    res.json({ msg: "show all courses" })
-})
-
-app.get("/api/v1/reviews", (req, res) => {
-    res.json({ msg: "show all reviews" })
-})
-
-app.post("/api/v1/courses", (req, res) => {
-    res.json({ msg: "add new course" })
-})
-
-app.post("/api/v1/reviews", (req, res) => {
-    res.json({ msg: "add new post" })
-})
-
-app.patch("/api/v1/courses/:id", (req, res) => {
-    res.json({ msg: "edit course" })
-})
-
-app.patch("/api/v1/reviews/:id", (req, res) => {
-    res.json({ msg: "edit review" })
-})
-
-app.delete("/api/v1/courses/:id", (req, res) => {
-    res.json({ msg: "delete course" })
-})
-
-app.delete("/api/v1/reviews/:id", (req, res) => {
-    res.json({ msg: "delete review" })
-})
-
-app.post("/api/v1/auth/register", (req, res) => {
-    res.json({ msg: "register user" })
-})
-
-app.post("/api/v1/auth/login", (req, res) => {
-    res.json({ msg: "login user" })
-})
-
+// Connect to database and start server
 const PORT = process.env.PORT || 8000
-
-app.listen(PORT, () => console.log(`running on port ${PORT}`))
+try {
+    await mongoose.connect(process.env.MONGO_URI)
+    app.listen(PORT, () => {
+        console.log(`server running on PORT ${PORT}`);
+    })
+} catch (error) {
+    console.log(error.message);
+    process.exit(1)
+}
