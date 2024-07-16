768120e5e659a4891f244401b9afb60d2e4fecbb
Kevin Dela Paz <kevindelapaz21@gmail.com>
2024-07-16 08:34:41 -0500

    created a seeder for the DB

---
+++ seeder.js
@@ -0,0 +1,50 @@
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import mongoose from 'mongoose'
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Load models
+import Bootcamp from './models/BootcampModel.js'
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI)
+
+// Get the current directory
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Read JSON files
+const bootcamps = JSON.parse(readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'))
+
+// Import to DB
+const importData = async () => {
+    try {
+        await Bootcamp.create(bootcamps)
+        console.log("Data imported");
+        process.exit()
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// Delete data
+const deleteData = async () => {
+    try {
+        await Bootcamp.deleteMany()
+        console.log("Data destroyed");
+        process.exit()
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+
+if (process.argv[2] === '-i') {
+    importData()
+} else if (process.argv[2] === '-d') {
+    deleteData()
+}
--- utils/geocoder.js
+++ utils/geocoder.js
@@ -1,4 +1,6 @@
 import node_geocoder from "node-geocoder";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const options = {
     provider: process.env.GEOCODER_PROVIDER,
