5be7f6cb9ce7d5335f37f04e824f15a2aab38d6c
Kevin Dela Paz <kevindelapaz21@gmail.com>
2024-07-15 22:13:13 -0500

    added slug for the location for the database

--- models/BootcampModel.js
+++ models/BootcampModel.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import slugify from "slugify";
+import geocoder from "../utils/geocoder.js";
 
 const BootcampSchema = new mongoose.Schema({
     name: {
@@ -97,5 +99,31 @@ const BootcampSchema = new mongoose.Schema({
         default: Date.now
     },
 });
+
+// Create bootcamp slug from the name
+BootcampSchema.pre("save", function (next) {
+    this.slug = slugify(this.name, { lower: true })
+    next()
+})
+
+// Geocode & create location field
+BootcampSchema.pre('save', async function (next) {
+    const loc = await geocoder.geocode(this.address)
+    this.location = {
+        type: 'Point',
+        coordinates: [loc[0].longitude, loc[0].latitude],
+        formattedAddress: loc[0].formattedAddress,
+        street: loc[0].streetName,
+        city: loc[0].city,
+        state: loc[0].stateCode,
+        zipcode: loc[0].zipcode,
+        country: loc[0].countryCode
+    }
+
+    // Do not save address in DB
+    this.address = undefined
+
+    next()
+})
 
 export default mongoose.model("Bootcamp", BootcampSchema)
---
+++ utils/geocoder.js
@@ -0,0 +1,11 @@
+import node_geocoder from "node-geocoder";
+
+const options = {
+    provider: process.env.GEOCODER_PROVIDER,
+    httpAdapter: 'https',
+    apiKey: process.env.GEOCODER_API_KEY,
+    formatter: null
+}
+
+const geocoder = node_geocoder(options)
+export default geocoder
