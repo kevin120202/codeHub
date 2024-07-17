3fa90bac68c1a5a88317eb846838265a2b9c8076
Kevin Dela Paz <kevindelapaz21@gmail.com>
2024-07-17 11:13:55 -0500

    added avg cost calculations

--- models/CourseModel.js
+++ models/CourseModel.js
@@ -16,7 +16,7 @@ const CourseScheme = new mongoose.Schema({
     },
     tuition: {
         type: Number,
-        required: [true, "Please adda tuition cose"]
+        required: [true, "Please add a tuition cost"]
     },
     minimumSkill: {
         type: String,
@@ -36,6 +36,40 @@ const CourseScheme = new mongoose.Schema({
         ref: "Bootcamp",
         required: true
     }
+})
+
+CourseScheme.statics.getAverageCost = async function (bootcampId) {
+    console.log("calculating avg code");
+
+    const obj = await this.aggregate([
+        {
+            $match: { bootcamp: bootcampId }
+        },
+        {
+            $group: {
+                _id: "$bootcamp",
+                averageCost: { $avg: "$tuition" }
+            }
+        }
+    ])
+
+    try {
+        await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+            averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+        })
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// Call getAverageCost after save
+CourseScheme.post("save", function () {
+    this.constructor.getAverageCost(this.bootcamp)
+})
+
+// Call getAverageCost before remove
+CourseScheme.pre("remove", function () {
+    this.constructor.getAverageCost(this.bootcamp)
 })
 
 export default mongoose.model("Course", CourseScheme)
